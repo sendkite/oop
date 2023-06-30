2e24d98c4ab9384a8527f20fb297e96fe69825c0
sendkite <sendkite1@gmail.com>
2023-06-30 13:03:16 +0900

    feat 할인 조건

--- src/theater/chapter02/step01/movie/price/PeriodCondition.java
+++ src/theater/chapter02/step01/movie/price/PeriodCondition.java
@@ -1,4 +1,25 @@
 package theater.chapter02.step01.movie.price;
 
-public class PeriodCondition {
+import theater.chapter02.step01.movie.DiscountCondition;
+import theater.chapter02.step01.movie.Screening;
+
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+
+public class PeriodCondition implements DiscountCondition {
+    private DayOfWeek dayOfWeek;
+    private LocalTime startTime;
+    private LocalTime endTime;
+
+    public PeriodCondition(DayOfWeek dayOfWeek, LocalTime startTime, LocalTime endTime) {
+        this.dayOfWeek = dayOfWeek;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public boolean isSatisfiedBy(Screening screening) {
+        return screening.getStartTime().getDayOfWeek().equals(dayOfWeek) &&
+                !startTime.isAfter(screening.getStartTime().toLocalTime()) &&
+                !endTime.isBefore(screening.getStartTime().toLocalTime());
+    }
 }
--- src/theater/chapter02/step01/movie/price/SequenceCondition.java
+++ src/theater/chapter02/step01/movie/price/SequenceCondition.java
@@ -1,4 +1,17 @@
 package theater.chapter02.step01.movie.price;
 
-public class SequenceCondition {
+import theater.chapter02.step01.movie.DiscountCondition;
+import theater.chapter02.step01.movie.Screening;
+
+public class SequenceCondition implements DiscountCondition {
+
+    private final int sequence;
+
+    public SequenceCondition(int sequence) {
+        this.sequence = sequence;
+    }
+
+    public boolean isSatisfiedBy(Screening screening) {
+        return screening.isSequence(sequence);
+    }
 }
