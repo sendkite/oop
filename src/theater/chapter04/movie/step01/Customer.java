d65b9488c63812cf6580876c99cf3d4536eb75b6
sendkite <sendkite1@gmail.com>
2023-07-06 22:52:03 +0900

    feat 데이터 중심 설계 -> 책임 중심으로 개선

---
+++ src/theater/chapter04/movie/step01/Customer.java
@@ -0,0 +1,4 @@
+package theater.chapter04.movie.step01;
+
+public class Customer {
+}
---
+++ src/theater/chapter04/movie/step01/DiscountConditionType.java
@@ -0,0 +1,6 @@
+package theater.chapter04.movie.step01;
+
+public enum DiscountConditionType {
+    SEQUENCE,       // 순번조건
+    PERIOD          // 기간 조건
+}
